d5f74651c767c735ab6e7337b39909198aed3d1a
Luyen Huynh <huynhdinhluyen@gmail.com>
2024-05-19 21:29:37 +0700

    add contructors and add auto increase for id

--- backend/src/main/java/com/example/backend/entity/Role.java
+++ backend/src/main/java/com/example/backend/entity/Role.java
@@ -1,18 +1,18 @@
 package com.example.backend.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "role")
 public class Role {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Integer id;
 
