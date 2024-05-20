920e151f8a11ba63382f824489bc83c477e184a4
Luyen Huynh <huynhdinhluyen@gmail.com>
2024-05-20 21:53:11 +0700

    create about page

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,146 @@
+import { Fade, Slide } from "@mui/material";
+
+export default function About() {
+  return (
+    <div className="container bg-gray-50 py-8 px-8 rounded-xl">
+      <Fade in={true} timeout={1000}>
+        <h1 className="font-semibold text-2xl text-center mb-4">GIỚI THIỆU</h1>
+      </Fade>
+      <Fade in={true} timeout={1000}>
+        <p className="italic">
+          <span className="font-semibold">DHL Diamond</span> từ lâu đã được biết
+          đến là một trong các thương hiệu uy tín tại thành phố Hồ Chí Minh
+          chuyên cung cấp trang sức kim cương chất lượng,
+          <span className="font-semibold"> GIÁ RẺ</span> cho cả nam và nữ, được
+          nhiều khách hàng tin tưởng lựa chọn.
+        </p>
+      </Fade>
+      <Fade in={true} timeout={1000}>
+        <div className="flex flex-col justify-center">
+          <img
+            src="https://i.pinimg.com/564x/6a/39/d2/6a39d2d094d9265033240231a6d9f9ce.jpg"
+            className="w-2/3 h-96 mt-4 rounded-xl mx-auto"
+            alt="about"
+          />
+
+          <p className="text-center italic">
+            Khách hàng luôn đặt niềm tin vào sản phẩm kim cương tại DHL Diamond
+          </p>
+        </div>
+      </Fade>
+      <Slide direction="up" in={true} timeout={1000}>
+        <div className="flex flex-col">
+          <h1 className="font-semibold text-xl mt-8">
+            QUÁ TRÌNH HÌNH THÀNH VÀ PHÁT TRIỂN
+          </h1>
+          <br />
+          <p>
+            DHL Diamond hình thành giữa thời buổi nền kinh tế của đất nước mở
+            cửa và có sự phát triển mạnh mẽ. Nhu cầu của con người đối với cuộc
+            sống ngày càng cao. Giờ đây ngoài ẩm thực, thời trang thì rất nhiều
+            người còn quan tâm tới cả trang sức, được biệt là trang sức cao cấp
+            làm từ kim cương.
+          </p>
+          <br />
+          <p>
+            Tại thị trường Việt Nam cũng có không ít đơn vị đang kinh doanh lĩnh
+            vực này. Tuy nhiên, mẫu mã sản phẩm còn chưa quá đa dạng, giá bán
+            lại chênh lệch hỗn loạn. Là những người yêu và có sự am hiểu về
+            trang sức kim cương, mong muốn được mang tới cho khách hàng những
+            mẫu trang sức không chỉ cao cấp, đẹp, sang trọng, thời thượng mà còn
+            là giá thành thấp, rẻ hơn so với thị trường nên chúng tôi đã thành
+            lập nên DHL Diamond.{" "}
+          </p>
+          <img
+            src="https://i.pinimg.com/564x/13/6b/e9/136be9f9f1afd202e8b7972950ca60de.jpg"
+            className="w-2/3 h-96 mt-4 rounded-xl mx-auto"
+            alt="shop"
+          />
+          <p className="text-center italic">
+            Cửa hàng trưng bày sản phẩm kim cương của DHL Diamond
+          </p>
+          <br />
+          <p>
+            Tại DHL Diamond khách hàng không chỉ có thể tìm được cho mình cho
+            mình rất nhiều mẫu trang sức kim cương đẹp và chất lượng, bảo đảm
+            100% thật, có giấy kiểm định quốc tế do GIA và IGI cấp. Ngoài trang
+            sức kim cương dành cho phái đẹp thì chúng tôi còn mang tới cả các
+            sản phẩm để phục vụ cho phái mạnh.{" "}
+          </p>
+          <br />
+          <p>
+            Mặc dù ban đầu DHL Diamond chỉ là một địa chỉ kinh doanh lẻ nhưng
+            sau nhiều năm không ngừng nỗ lực, hiện chúng tôi không chỉ trở thành
+            địa chỉ bán trang sức kim cương chính hãng, uy tín mà còn là đơn vị
+            phân phối trang sức kim cương cho rất nhiều cửa hàng trang sức đang
+            hoạt động tại TPHCM và các tỉnh thành khác. Bên cạnh đó, với nguồn
+            kim cương lớn được nhập khẩu từ Hồng Kông, DHL Diamond tự tin đáp
+            ứng mọi nhu cầu, thị hiếu của khách hàng từ mẫu mã cho đến{" "}
+            <span className="font-semibold">
+              giá thành hấp dẫn nhất Sài Gòn
+            </span>
+            .
+          </p>
+          <img
+            src="https://caohungdiamond.com/wp-content/uploads/2022/06/chung-nhan-chat-luong-san-pham-tai-cao-hung-diamond.jpg"
+            className="w-2/3 h-96 mt-4 rounded-xl mx-auto"
+            alt="gia-certificate"
+          />
+          <p className="text-center italic">
+            Chứng nhận của trang sức kim cương tại DHL Diamond
+          </p>
+        </div>
+      </Slide>
+      <Slide direction="up" in={true} timeout={1500}>
+      <div className="flex flex-col">
+        <h1 className="font-semibold text-xl mt-8">TẦM NHÌN VÀ SỨ MỆNH</h1>
+        <br />
+        <p>
+          Ngay từ khi bắt đầu, chúng tôi đã mang trên mình sứ mệnh cao cả đó là
+          tạo nên những giá trị sống đặc biệt cho từng khách hàng, và hơn bất cứ
+          điều gì đó chính là cá tính và độc đáo của từng sản phẩm. Chúng tôi
+          cam kết mang đến cho quý vị những sản phẩm sang trọng, hiện đại,
+          “chất” và hoàn toàn khác biệt. Một đơn vị được đánh giá là thành công
+          khi nhận được nhiều nhất sự tín nhiệm, tin cậy và ủng hộ của khách
+          hàng. Thấu hiểu một cách sâu sắc vấn đề đó, DHL DIAMOND luôn truyền
+          cảm hứng cho tất cả các thành viên trong tập thể, để bất cứ ai cũng
+          luôn có được sự chăm chút sản phẩm đến từng chi tiết nhỏ nhất. Mọi
+          giới hạn sẽ được phá vỡ để tạo nên những dịch vụ tốt nhất cho khách
+          hàng.
+        </p>
+      </div>
+      </Slide>
+      <Slide direction="up" in={true} timeout={2000}>
+      <div className="flex flex-col">
+        <h1 className="font-semibold text-xl mt-8">CHẤT LƯỢNG DỊCH VỤ</h1>
+        <br />
+        <p>
+          Với “kim chỉ nam” phục vụ khách hàng, đề cao chất lượng trong từng sản
+          phẩm và dịch vụ để có lộ trình phát triển bền vững nhất. Dưới bàn tay
+          của những người thợ kim hoàn tại DHL Diamond luôn linh hoạt, công phu,
+          tỉ mỉ đã tạo ra những sản phẩm đẹp sắc sảo, tinh tế trong kiểu dáng,
+          sang trọng, quý phái và hiện đại sẽ làm hài lòng mọi khách hàng khó
+          tính nhất. Môi trường làm việc sáng tạo, thoải mái và đánh thức các
+          giá trị tiềm ẩn của từng cá nhân trong công ty chính là chìa khóa giúp
+          DHL Diamond quy tụ các nhân tài có chuyên môn cao trong lĩnh vực của
+          mình và mong muốn được gắn bó lâu dài. Ngoài ra, DHL Diamond còn mang
+          đến những chính sách hậu mãi tốt nhất khi Quý khách có nhu cầu bảo
+          hành, đổi trả. Thủ tục đơn giản. Thời gian nhanh chóng. Nhân viên phục
+          vụ tận tình. Điều đó đã làm nên một DHL Diamond đã, đang và sẽ trường
+          tồn cùng thời gian.”
+        </p>
+        <br />
+        <p>
+          Với những nỗ lực không ngừng, trong tương lai, DHL Diamond chắc chắn
+          sẽ còn phát triển mạnh mẽ hơn nữa và đạt được những thành tựu nổi bật.
+          Trở thành địa chỉ số 1 trong lòng khách hàng khi có nhu cầu mua trang
+          sức kim cương chính hãng và là nhà phân phối trang sức kim cương hàng
+          đầu Việt Nam cũng như khu vực Đông Nam Á. Bên cạnh đó, ngày càng cải
+          tiến về dịch vụ tư vấn cho khách hàng chọn được viên kim cương ưng ý,
+          có thông số ý nghĩa, tăng lộc phát.
+        </p>
+      </div>
+      </Slide>
+    </div>
+  );
+}
