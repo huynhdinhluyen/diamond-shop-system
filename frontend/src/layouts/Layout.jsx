d07864fc03ea1024dd24d0f8ca94e2d64d16a4f0
Luyen Huynh <huynhdinhluyen@gmail.com>
2024-05-20 16:15:04 +0700

    Update Layout.jsx

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -5,7 +5,7 @@ export default function Layout({children}) {
   return (
     <div className="flex flex-col min-h-screen">
         <Header />
-        <div className="container mx-auto flex-1 py-10">{children}</div>
+        <div className="mx-auto flex-1">{children}</div>
         <Footer />
     </div>
   )
