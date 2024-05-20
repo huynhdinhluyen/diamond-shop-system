5586eac89d92d6bae9dc8a4adbcec0cfa409c632
Luyen Huynh <huynhdinhluyen@gmail.com>
2024-05-20 08:33:44 +0700

    create layout

---
+++ frontend/src/layouts/Layout.jsx
@@ -0,0 +1,12 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+export default function Layout({children}) {
+  return (
+    <div className="flex flex-col min-h-screen">
+        <Header />
+        <div className="container mx-auto flex-1 py-10">{children}</div>
+        <Footer />
+    </div>
+  )
+}
