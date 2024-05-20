4dad1433ca3bd098dc79aa4ab74e908f9c02e5c8
Luyen Huynh <huynhdinhluyen@gmail.com>
2024-05-20 16:32:21 +0700

    Update ProductCategory.jsx

--- frontend/src/components/ProductCategory.jsx
+++ frontend/src/components/ProductCategory.jsx
@@ -27,26 +27,39 @@ export default function ProductCategory() {
     return <CircularProgress />;
   }
   return (
-    <div className="container mx-auto flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 md:space-x-8 mb-8">
-      {categories.map((category) => (
-        <Grow in={true} timeout={1000} key={category.id}>
-          <Box className="flex flex-col items-center mx-8">
-            <img
-              src={category.imageUrl}
-              alt={category.name}
-              className="w-52 h-52 mr-2 rounded-full transition-transform duration-300 hover:scale-105"
-            />
-            <Typography
-              variant="body1"
-              component={Link}
-              to={`/categories/${category.name}`}
-              className="!text-xl"
-            >
-              {category.name}
-            </Typography>
-          </Box>
-        </Grow>
-      ))}
+    <div className="container mx-auto mb-8">
+      <div className="flex items-center justify-center mb-4">
+        <div className="w-96 border-t border-gray-300"></div>
+        <Typography
+          variant="h4"
+          component="h4"
+          className="text-center font-bold mb-4 !mx-8"
+        >
+          DANH MỤC SẢN PHẨM
+        </Typography>
+        <div className="w-96 border-t border-gray-300"></div>
+      </div>
+      <div className="flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 md:space-x-8">
+        {categories.map((category) => (
+          <Grow in={true} timeout={1000} key={category.id}>
+            <Box className="flex flex-col items-center mx-8">
+              <img
+                src={category.imageUrl}
+                alt={category.name}
+                className="w-52 h-52 mr-2 rounded-full transition-transform duration-300 hover:scale-105"
+              />
+              <Typography
+                variant="body1"
+                component={Link}
+                to={`/categories/${category.name}`}
+                className="!text-xl"
+              >
+                {category.name}
+              </Typography>
+            </Box>
+          </Grow>
+        ))}
+      </div>
     </div>
   );
 }
