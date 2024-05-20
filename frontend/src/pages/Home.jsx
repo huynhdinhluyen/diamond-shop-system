941f574a9fc3c7d7b0d298ec8310b313613e263e
Luyen Huynh <huynhdinhluyen@gmail.com>
2024-05-20 16:14:54 +0700

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,13 @@
+import Banner from "../components/Banner";
+import Benefits from "../components/Benefits";
+import ProductCategory from "../components/ProductCategory";
 
 export default function Home() {
   return (
-    <div className="container"></div>
-  )
+    <div>
+      <Banner />
+      <Benefits />
+      <ProductCategory />
+    </div>
+  );
 }
