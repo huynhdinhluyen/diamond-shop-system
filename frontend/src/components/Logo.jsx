35e639b722b8e0246d0d0338c9cbfbd81ff4033f
Luyen Huynh <huynhdinhluyen@gmail.com>
2024-05-20 17:33:15 +0700

    add logo

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,6 +1,11 @@
-
 export default function Logo() {
   return (
-    <div className="flex items-center mx-auto">Logo</div>
-  )
+    <div className="flex items-center mx-auto">
+      <img
+        src="https://i.pinimg.com/564x/5c/ce/97/5cce976cdbda0e2560091cb55f12fcbb.jpg"
+        className="w-20 h-20 sm:w-24 sm:h-24"
+        alt="logo"
+      />
+    </div>
+  );
 }
