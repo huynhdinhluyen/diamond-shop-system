5a0a796abf9e4a0d6fe935f037bed3d250039542
Nhat Hoang <29.hoang.10@gmail.com>
2024-05-21 14:03:23 +0700

    update Layout.jsx

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -1,12 +1,17 @@
+/* eslint-disable react/prop-types */
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Newsletter from "../components/Newsletter";
+import Slider from "../components/Slider";
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
-        <Header />
-        <div className="container mx-auto flex-1 py-10">{children}</div>
-        <Footer />
+      <Header />
+      <Slider />
+      <div className="container mx-auto flex-1 py-10">{children}</div>
+      <Newsletter />
+      <Footer />
     </div>
-  )
+  );
 }
