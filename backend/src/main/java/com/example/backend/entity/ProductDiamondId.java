54c8a66df3e83ecc6d239183e0496d9722e6eaad
Luyen Huynh <huynhdinhluyen@gmail.com>
2024-05-21 23:24:20 +0700

    Update ProductDiamondId.java

--- backend/src/main/java/com/example/backend/entity/ProductDiamondId.java
+++ backend/src/main/java/com/example/backend/entity/ProductDiamondId.java
@@ -2,35 +2,24 @@ package com.example.backend.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.Hibernate;
+import java.io.Serializable;
 
-import java.util.Objects;
 
 @Getter
 @Setter
 @Embeddable
-public class ProductDiamondId implements java.io.Serializable {
-    private static final long serialVersionUID = 704129919662721502L;
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDiamondId implements Serializable {
+
     @Column(name = "product_id", nullable = false)
     private Integer productId;
 
     @Column(name = "diamond_id", nullable = false)
     private Integer diamondId;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        ProductDiamondId entity = (ProductDiamondId) o;
-        return Objects.equals(this.productId, entity.productId) &&
-                Objects.equals(this.diamondId, entity.diamondId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(productId, diamondId);
-    }
 
 }
