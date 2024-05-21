ebb699c3bb83ad4a329129be35571337329bb287
Luyen Huynh <huynhdinhluyen@gmail.com>
2024-05-21 23:26:02 +0700

    Update ProductDTO.java

--- backend/src/main/java/com/example/backend/dto/ProductDTO.java
+++ backend/src/main/java/com/example/backend/dto/ProductDTO.java
@@ -5,14 +5,24 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductDTO {
     private Integer id;
+    private DiamondCasingDTO diamondCasing;
     private String name;
     private String imageUrl;
-    private Long costPrice;
+    private Long laborCost;
+    private BigDecimal profitMargin;
+    private Integer stockQuantity;
+    private PromotionDTO promotion;
+    private WarrantyDTO warranty;
+    private DiamondDTO mainDiamond;
+    private List<DiamondDTO> auxiliaryDiamondDTOs;
     private Long salePrice;
 }
