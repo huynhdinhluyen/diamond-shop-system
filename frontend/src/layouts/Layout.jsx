04cf81de365116303cd9f335b0d5d150dc3e52a2
Luyen Huynh <huynhdinhluyen@gmail.com>
2024-05-21 16:10:13 +0700

    refactor

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -2,17 +2,11 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Newsletter from "../components/Newsletter";
-import Slider from "../components/Slider";
 
 export default function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
-        <Header />
-        <div className="mx-auto flex-1">{children}</div>
-        <Footer />
-
       <Header />
-      <Slider />
       <div className="container mx-auto flex-1 py-10">{children}</div>
       <Newsletter />
       <Footer />
