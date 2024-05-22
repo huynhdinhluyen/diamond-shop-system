7672ad9aefe034319ab5c7434dc510b502d22910
Luyen Huynh <huynhdinhluyen@gmail.com>
2024-05-22 10:42:34 +0700

    Create ProductDiamondRepository.java

---
+++ backend/src/main/java/com/example/backend/repository/ProductDiamondRepository.java
@@ -0,0 +1,9 @@
+package com.example.backend.repository;
+
+import com.example.backend.entity.ProductDiamond;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductDiamondRepository extends JpaRepository<ProductDiamond, Integer> {
+}
