09ee7bfec80aa4d237ebef46538a0e71f64ca8c8
Nhat Hoang <29.hoang.10@gmail.com>
2024-05-22 16:42:18 +0700

    Update Logo.jsx

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function Logo() {
   return (
     <div className="flex lg:justify-normal justify-center">
-      <a href="#">
+      <Link to="/">
         <img
           className="w-[250px] h-[60px] object-cover"
           src="/public/assets/img/logo/logo.svg"
           alt=""
         />
-      </a>
+      </Link>
     </div>
   );
 }
