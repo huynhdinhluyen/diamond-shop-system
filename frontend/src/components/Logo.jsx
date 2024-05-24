39f6855f3d48c8ccfd20c61017c2cc51ed89f44a
Nhat Hoang <29.hoang.10@gmail.com>
2024-05-24 15:04:56 +0700

    Fix Logo.jsx

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -2,14 +2,12 @@ import { Link } from "react-router-dom";
 
 export default function Logo() {
   return (
-    <div className="flex lg:justify-normal justify-center">
-      <Link to="/">
-        <img
-          className="w-[250px] h-[60px] object-cover"
-          src="/public/assets/img/logo/logo.svg"
-          alt=""
-        />
-      </Link>
-    </div>
+    <Link to="/" className="flex lg:justify-normal justify-center">
+      <img
+        className="w-[250px] h-[60px] object-cover"
+        src="/public/assets/img/logo/logo.svg"
+        alt=""
+      />
+    </Link>
   );
 }
