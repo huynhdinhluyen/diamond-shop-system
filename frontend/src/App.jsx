93c8bb8a90c9da7ebbb19068ee7a7d26e14ec426
Nhat Hoang <29.hoang.10@gmail.com>
2024-05-26 11:13:52 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,12 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+import News from "./pages/News";
+import NewsDetails from "./pages/NewsDetails";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -22,7 +28,36 @@ function App() {
           </Layout>
         }
       />
+
+      <Route
+        path="/news"
+        element={
+          <Layout>
+            <News />
+          </Layout>
+        }
+      />
+
+      <Route
+        path="/news/:sectionId"
+        element={
+          <Layout>
+            <NewsDetails />
+          </Layout>
+        }
+      />
+      <Route
+        path="/contact"
+        element={
+          <Layout>
+            <Contact />
+          </Layout>
+        }
+      />
       <Route path="*" element={<Navigate to="/" />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 }
