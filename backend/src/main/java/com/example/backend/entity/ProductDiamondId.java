9a18b15b19481f37a34d9705b750e7b9cc6731fd
Luyen Huynh <huynhdinhluyen@gmail.com>
2024-05-28 14:35:05 +0700

    Update ProductDiamondId.java

--- backend/src/main/java/com/example/backend/entity/ProductDiamondId.java
+++ backend/src/main/java/com/example/backend/entity/ProductDiamondId.java
@@ -1,13 +1,13 @@
 package com.example.backend.entity;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.Hibernate;
 import java.io.Serializable;
-
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -15,11 +15,22 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductDiamondId implements Serializable {
-
     @Column(name = "product_id", nullable = false)
     private Integer productId;
-
     @Column(name = "diamond_id", nullable = false)
     private Integer diamondId;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        ProductDiamondId entity = (ProductDiamondId) o;
+        return Objects.equals(this.productId, entity.productId) &&
+                Objects.equals(this.diamondId, entity.diamondId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, diamondId);
+    }
 }
