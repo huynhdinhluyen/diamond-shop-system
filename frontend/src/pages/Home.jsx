f0bc898956019cb1c73c148b267f21392c1f14d1
Nhat Hoang <29.hoang.10@gmail.com>
2024-05-28 23:29:49 +0700

    Add Slider.jsx in Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import Benefits from "../components/Benefits";
 import ProductCategory from "../components/ProductCategory";
 import ProductList from "../components/ProductList";
+import News from "../components/News";
 import Slider from "../components/Slider";
 export default function Home() {
   return (
-    <div>
+    <div className="relative">
       <Slider />
       <Benefits />
       <ProductCategory />
       <ProductList />
+      <News />
     </div>
   );
 }
