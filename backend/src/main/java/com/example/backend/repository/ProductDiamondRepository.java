37e82410d6e74435df154ead3a8c74200aedae87
Luyen Huynh <huynhdinhluyen@gmail.com>
2024-05-28 14:38:23 +0700

    add deleteByProduct

--- backend/src/main/java/com/example/backend/repository/ProductDiamondRepository.java
+++ backend/src/main/java/com/example/backend/repository/ProductDiamondRepository.java
@@ -1,9 +1,11 @@
 package com.example.backend.repository;
 
+import com.example.backend.entity.Product;
 import com.example.backend.entity.ProductDiamond;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductDiamondRepository extends JpaRepository<ProductDiamond, Integer> {
+    void deleteByProduct(Product product);
 }
