cf13a7e243ace3109669fed6f7df2b6f2aa69b85
Luyen Huynh <huynhdinhluyen@gmail.com>
2024-05-28 14:34:26 +0700

    Update ProductRepository.java

--- backend/src/main/java/com/example/backend/repository/ProductRepository.java
+++ backend/src/main/java/com/example/backend/repository/ProductRepository.java
@@ -6,9 +6,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     @Query("SELECT p FROM Product p JOIN FETCH p.diamondCasing dc JOIN FETCH p.productDiamonds pd JOIN FETCH pd.diamond d")
-    List<Product> findAll();
+    List<Product> findAllWithDetails();
+
+    @Query("SELECT p FROM Product p " +
+            "JOIN FETCH p.diamondCasing " +
+            "JOIN FETCH p.productDiamonds pd " +
+            "JOIN FETCH pd.diamond " +
+            "WHERE p.id = :id")
+    Optional<Product> findByIdWithDetails(Integer id);
 }
