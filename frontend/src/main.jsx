92975b022d1c50570cc8eeaf9a427e21a542d4e0
Nhat Hoang <29.hoang.10@gmail.com>
2024-05-29 12:14:46 +0700

    Update Main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,11 +6,27 @@ import "./index.css";
 // import "@fontsource/roboto/500.css";
 // import "@fontsource/roboto/700.css";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./hooks/useAuth.jsx";
+import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // {/* <React.StrictMode> */}
   <BrowserRouter>
-    <App />
+    <AuthProvider>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <App />
+    </AuthProvider>
   </BrowserRouter>
   // {/* </React.StrictMode> */}
 );
