2bb66bcdd02a86cbe54ae3f63aa52b819a8d760a
Nhat Hoang <29.hoang.10@gmail.com>
2024-05-30 16:30:14 +0700

    Update AuthenticationResponse

--- backend/src/main/java/com/example/backend/entity/AuthenticationResponse.java
+++ backend/src/main/java/com/example/backend/entity/AuthenticationResponse.java
@@ -1,17 +1,28 @@
 package com.example.backend.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AuthenticationResponse {
     private String token;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String city;
 
-    public AuthenticationResponse(String token) {
+    public AuthenticationResponse(String token, User user) {
         this.token = token;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
+        this.username = user.getUsername();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.email = user.getEmail();
+        this.phoneNumber = user.getPhoneNumber();
+        this.address = user.getAddress();
+        this.city = user.getCity();
     }
 }
