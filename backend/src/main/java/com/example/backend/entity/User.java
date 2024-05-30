df81724e4ebc4b39874749164ea9e5f87eb8bda4
Nhat Hoang <29.hoang.10@gmail.com>
2024-05-30 13:21:24 +0700

    add authentication

--- backend/src/main/java/com/example/backend/entity/User.java
+++ backend/src/main/java/com/example/backend/entity/User.java
@@ -3,6 +3,12 @@ package com.example.backend.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.Nationalized;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -13,7 +19,7 @@ import org.hibernate.annotations.Nationalized;
         @UniqueConstraint(columnNames = "email"),
         @UniqueConstraint(columnNames = "phone_number")
 })
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
@@ -47,9 +53,10 @@ public class User {
     @Column(name = "address")
     private String address;
 
+
     @Enumerated(EnumType.STRING)
     @Column(name = "role", nullable = false, length = 20)
-    private RoleName roleName;
+    private RoleName roleName = RoleName.CUSTOMER;
 
     public enum RoleName {
         ADMIN,
@@ -57,5 +64,30 @@ public class User {
         SALES_STAFF,
         DELIVERY_STAFF,
         MANAGER
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(roleName.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
