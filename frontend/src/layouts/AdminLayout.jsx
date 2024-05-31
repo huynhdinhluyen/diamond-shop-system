15615a7bdc8f0523e78a1d10467ada2da1f3e445
Nhat Hoang <29.hoang.10@gmail.com>
2024-05-31 13:31:48 +0700

    Update AdminLayout.jsx

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import AdminHeader from "../components/AdminHeader";
 import AdminSidebar from "../components/AdminSidebar";
+import { useAuth } from "../hooks/useAuth";
 
 function AdminLayout() {
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const { user } = useAuth();
 
   const toggleDrawer = () => {
     setDrawerOpen(!drawerOpen);
@@ -15,9 +17,10 @@ function AdminLayout() {
       <AdminSidebar open={drawerOpen} onClose={toggleDrawer} />
       <div className="flex flex-col w-full">
         <AdminHeader onToggleSidebar={toggleDrawer} />
-        <main className="container flex-grow p-4 mx-auto">
-          <Outlet />
-        </main>
+        {user && user.role === "ADMIN" ? (
+          <main className="container flex-grow p-4 mx-auto">
+            <Outlet />
+          </main>) : (<div className="text-center mt-8">Bạn không có quyền truy cập!</div>)}
       </div>
     </div>
   );
