79fd903a98b42dfe21f3b48e3d9a892f3a563d2d
Nhat Hoang <29.hoang.10@gmail.com>
2024-05-31 11:18:58 +0700

    Update useAuth.jsx

--- frontend/src/hooks/useAuth.jsx
+++ frontend/src/hooks/useAuth.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
 import { createContext, useContext, useState } from "react";
 import * as userService from "../service/userService";
@@ -6,16 +7,32 @@ import "react-toastify/dist/ReactToastify.css";
 
 const AuthContext = createContext(null);
 export const AuthProvider = ({ children }) => {
+
   const [user, setUser] = useState(userService.getUser());
-  const login = async (email, password) => {
+
+  const login = async (username, password) => {
     try {
-      const user = await userService.login(email, password);
+      const user = await userService.login(username, password);
       setUser(user);
       toast.success("Login Successfully");
+      return true;
     } catch (err) {
-      toast.error("Invalid Email or Password");
+      toast.error("Invalid Username or Password");
+      return false;
     }
   };
+
+  const register = async (data) => {
+    try {
+      const user = await userService.register(data);
+      setUser(user);
+      toast.success("Đăng ký thành công, mời bạn đăng nhập!");
+      return true;
+    } catch (err) {
+      toast.error(err.response.data || "Đăng ký thất bại!")
+      return false;
+    }
+  }
 
   const logout = async () => {
     await userService.logout();
@@ -24,7 +41,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ user, login, logout, register }}>
       {children}
     </AuthContext.Provider>
   );
