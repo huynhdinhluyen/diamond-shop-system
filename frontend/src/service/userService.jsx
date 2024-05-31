fb25cf3ae9e224ae3c19748772e4b3d26aaa1fa2
Nhat Hoang <29.hoang.10@gmail.com>
2024-05-31 11:19:50 +0700

    Update userService.jsx

--- frontend/src/service/userService.jsx
+++ frontend/src/service/userService.jsx
@@ -5,14 +5,20 @@ export const getUser = () =>
     ? JSON.parse(localStorage.getItem("user"))
     : null;
 
-export const login = async (email, password) => {
+export const login = async (username, password) => {
   const { data } = await axiosInstance.post("api/users/login", {
-    email,
+    username,
     password,
   });
   localStorage.setItem("user", JSON.stringify(data));
   return data;
 };
+
+export const register = async (registerData) => {
+  const { data } = await axiosInstance.post("api/users/register", registerData);
+  localStorage.setItem("user", JSON.stringify(data));
+  return data;
+}
 
 export const logout = () => {
   localStorage.removeItem("user");
