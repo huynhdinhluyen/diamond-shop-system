42f16a019ca9af2aeb78b9e49c798fe61e85d365
Nhat Hoang <29.hoang.10@gmail.com>
2024-06-05 11:57:46 +0700

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,25 +8,28 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./hooks/useAuth.jsx";
 import { ToastContainer } from "react-toastify";
+import CartProvider from "./hooks/useCart.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // {/* <React.StrictMode> */}
   <BrowserRouter>
     <AuthProvider>
-      <ToastContainer
-        position="bottom-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-      <App />
+      <CartProvider>
+        <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+        <App />
+      </CartProvider>
     </AuthProvider>
-  </BrowserRouter>
+  </BrowserRouter >
   // {/* </React.StrictMode> */}
 );
