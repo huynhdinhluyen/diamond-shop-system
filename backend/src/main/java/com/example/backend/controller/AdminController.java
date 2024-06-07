793af0293e3d82a8795b4191e18790eb8b6218d2
Dvil165 <baoduytran1605@gmail.com>
2024-06-07 11:27:31 +0700

    .

--- backend/src/main/java/com/example/backend/controller/AdminController.java
+++ backend/src/main/java/com/example/backend/controller/AdminController.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/api/admin")
 public class AdminController {
     @Autowired
     @Qualifier("orderServiceImpl")
