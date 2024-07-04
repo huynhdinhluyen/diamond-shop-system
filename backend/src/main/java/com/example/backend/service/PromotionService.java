4260a2b1d19decc4835afc957241a9c8332db2e3
Dvil165 <baoduytran1605@gmail.com>
2024-07-05 00:30:49 +0700

    .

--- backend/src/main/java/com/example/backend/service/PromotionService.java
+++ backend/src/main/java/com/example/backend/service/PromotionService.java
@@ -1,6 +1,7 @@
 package com.example.backend.service;
 
 import com.example.backend.dto.PromotionDTO;
+import com.example.backend.response.PromotionDeleteResponse;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,4 +11,8 @@ public interface PromotionService {
     List<PromotionDTO> getAllPromotions();
 
     PromotionDTO addPromotion(PromotionDTO promotion);
+
+    PromotionDTO updatePromotion(Integer PromotionID ,PromotionDTO promotion);
+
+    void deletePromotion(Integer PromotionID);
 }
