8953cafa17abf3592344fe82bb1ecf95b0b325f1
Dvil165 <baoduytran1605@gmail.com>
2024-07-04 19:34:43 +0700

    .

--- backend/src/main/java/com/example/backend/service/AuthenticationService.java
+++ backend/src/main/java/com/example/backend/service/AuthenticationService.java
@@ -23,7 +23,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.Date;
-import java.util.UUID;
 
 @Service
 public class AuthenticationService {
